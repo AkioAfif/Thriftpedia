2345fe4c216b441fcc0a320615138fdeab369c1f
Akio Afifian Ahsan <akioafif1@gmail.com>
2026-09-26 21:25:15 +0700

    feat(order): tambahkan validasi id pesanan, query list, dan update status

--- server/src/validators/order.validator.js
+++ server/src/validators/order.validator.js
@@ -1,4 +1,5 @@
 const AppError = require('../utils/AppError');
+const { ORDER_STATUS, ADMIN_SETTABLE_STATUSES } = require('../constants/order');
 
 // Strict 24-hex check. Do NOT use mongoose.isValidObjectId: it accepts any 12-char string.
 const OBJECT_ID_RE = /^[a-f\d]{24}$/i;
@@ -12,4 +13,31 @@ function validateCreateOrder(req, res, next) {
   next();
 }
 
-module.exports = { validateCreateOrder };
+function validateOrderIdParam(req, res, next) {
+  if (!isObjectIdString(req.params.orderId)) {
+    return next(new AppError(400, 'orderId must be a valid id'));
+  }
+  next();
+}
+
+function validateListQuery(req, res, next) {
+  const { status } = req.query;
+  if (status !== undefined && !Object.values(ORDER_STATUS).includes(status)) {
+    return next(new AppError(400, `status must be one of ${Object.values(ORDER_STATUS).join(', ')}`));
+  }
+  next();
+}
+
+function validateUpdateStatus(req, res, next) {
+  const body = req.body ?? {};
+  const keys = Object.keys(body);
+  if (keys.length !== 1 || keys[0] !== 'status') {
+    return next(new AppError(400, 'Only the "status" field can be updated'));
+  }
+  if (!ADMIN_SETTABLE_STATUSES.includes(body.status)) {
+    return next(new AppError(400, `status must be one of ${ADMIN_SETTABLE_STATUSES.join(', ')}`));
+  }
+  next();
+}
+
+module.exports = { validateCreateOrder, validateOrderIdParam, validateListQuery, validateUpdateStatus };
